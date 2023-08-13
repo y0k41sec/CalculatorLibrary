425711aae34a72ddc5312cf91d3eb4ea4a82dbc0
Jonathan Goya Nogiri <jonathannogiri@gmail.com>
2023-08-13 15:45:32 +0100

    Adding calculator functions

---
+++ calculator.py
@@ -0,0 +1,11 @@
+"""
+Calculator library containing basic math operations.
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def substract(first_term, second_term):
+    return first_term - second_term     
+
+    
